5c42e50f7aa05ce0e48ab6828029250785cee333
Danny W <seldon.wilson@gmail.com>
2019-07-12 19:25:23 -0700

    Added Master Manager
    Created a singleton type for scriptable objects, of which MasterManager belongs. This manager holds onto the game settings and when the TestConnect script runs, it sets up the settings for the connection based on the MasterManager's reference to the GameSettings scriptable object (non-singleton because you may want to have additional setttings, such as different profiles or accounts).

---
+++ Assets/Scripts/TestConnect.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+   private void Start()
+   {
+      print("Connecting to server...");
+      PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+      PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+      PhotonNetwork.ConnectUsingSettings();
+   }
+
+   public override void OnConnectedToMaster()
+   {
+      print("Connected to server : )");
+      print(PhotonNetwork.LocalPlayer.NickName);
+   }
+
+   public override void OnDisconnected(DisconnectCause cause)
+   {
+      print("Disconnected from server for reason " + cause.ToString());
+   }
+}
